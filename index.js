70a1cbaf22aeb16634bb40003b2fc85bfcdb0e9f
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-10 10:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ speak(){
   return `${this.name} says meow!`
 }
 }
+
+class Dog{
+  constructor(name,sex){
+    this.name=name
+    this.sex=sex
+  }
+  
+
+speak(){
+  return `${this.name} says woof!`
+}
+}
+
+class Bird{
+  constructor(name,sex){
+    this.name=name
+    this.sex=sex
+  }
+  
+if (sex=male){
+speak(){
+  return `it's me!${this.name} the parrot!`
+}
+}
+if(sex!=male){
+  
+}
+}
